106f2aa0742485563c17c21b4713418416695819
Tyler Twining <twijn@twijn.net>
2022-08-21 14:04:55 -0400

    Add getUser WS route

--- wsRoutes.js
+++ wsRoutes.js
@@ -2,12 +2,17 @@ module.exports = {
     send: (message, reply, socket) => {
         if (message.hasOwnProperty("message")) {
             if (message.hasOwnProperty("channel") && message.hasOwnProperty("guild")) {
-
+                //TODO
             } else if (message.hasOwnProperty("recipient")) {
                 if (socket.allowed.users.includes(message.recipient)) {
                     global.discord.users.fetch(message.recipient).then(user => {
                         user.send(message.message).then(msg => {
-                            reply({success: true, message: msg});
+                            reply({success: true, message: {
+                                id: msg.id,
+                                content: msg.content,
+                                embeds: msg.embeds,
+                                components: msg.components
+                            }});
                         }, err => {
                             reply({success: false, error: err.rawError})
                         });
@@ -23,5 +28,16 @@ module.exports = {
         } else {
             reply({success: false, error: "Invalid body, missing message"});
         }
-    }
+    },
+    getUser: (message, reply, socket) => {
+        if (message.hasOwnProperty("id")) {
+            global.discord.users.fetch(message.id).then(user => {
+                reply({success: true, user: user});
+            }, err => {
+                reply({success: false, error: err.rawError});
+            });
+        } else {
+            reply({success: false, error: "Invalid body, missing ID"});
+        }
+    },
 }
