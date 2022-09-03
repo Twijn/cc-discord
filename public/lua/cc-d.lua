dfe48c748b3b9ba647316575eed86cb4a7bfbc3f
Tyler Twining <twijn@twijn.net>
2022-09-03 18:14:54 -0400

    Embed builders & other improvements

--- public/lua/cc-d.lua
+++ public/lua/cc-d.lua
@@ -4,7 +4,111 @@ local NONCE_LENGTH = 8
 local RETRY_INTERVAL = 5
 local MAX_RETRIES = 20
 
-local CCD = {}
+local buttonStyles = {
+    PRIMARY = 1,
+    SECONDARY = 2,
+    SUCCESS = 3,
+    DANGER = 4,
+    LINK = 5,
+}
+
+local CCD = {
+
+    EmbedBuilder = function(data)
+        local _ = {
+            data = data or {}
+        }
+
+        function _.addField(field) if not _.data.fields then _.data.fields = {} end table.insert(_.data.fields, field) return _ end
+        function _.setAuthor(author) _.data.author = author return _ end
+        function _.setColor(color) _.data.color = color return _ end
+        function _.setDescription(description) _.data.description = description return _ end
+        function _.setFields(fields) _.data.fields = fields return _ end
+        function _.setFooter(footer) _.data.footer = footer return _ end
+        function _.setImage(image) _.data.image = image return _ end
+        function _.setThumbnail(thumbnail) _.data.thumbnail = thumbnail return _ end
+        function _.setTimestamp(timestamp) _.data.timestamp = timestamp return _ end
+        function _.setTitle(title) _.data.title = title return _ end
+        function _.setURL(url) _.data.url = url return _ end
+        
+        function _.build() return _.data end
+
+        return _
+    end,
+    ActionRowBuilder = function(data)
+        local _ = {
+            data = data or {}
+        }
+        _.data.type = 1
+
+        _.data.components = _.data.components or {}
+
+        function _.addComponent(component) if component.data then component = component.build() end table.insert(_.data.components, component) return _ end
+        function _.addComponents(...) for i,v in pairs ({...}) do _.addComponent(v) end return _ end
+        function _.setComponents(components) _.data.components = components return _ end
+        
+        function _.build() return _.data end
+
+        return _
+    end,
+    SelectMenuOptionBuilder = function(data)
+        local _ = {
+            data = data or {}
+        }
+
+        function _.setDefault(default) _.data.default = default return _ end
+        function _.setDescription(description) _.data.description = description return _ end
+        function _.setEmoji(emoji) _.data.emoji = emoji return _ end
+        function _.setLabel(label) _.data.label = label return _ end
+        function _.setValue(value) _.data.value = value return _ end
+        
+        function _.build() return _.data end
+
+        return _
+    end,
+    SelectMenuBuilder = function(data)
+        local _ = {
+            data = data or {}
+        }
+        _.data.type = 3
+
+        _.data.options = _.data.options or {}
+
+        function _.addOption(option) if option.data then option = option.build() end table.insert(_.data.options, option) return _ end
+        function _.addOptions(...) local args = {...} for i,v in pairs(args) do _.addOption(v) end return _ end
+        function _.setCustomId(customId) _.data.customId = customId return _ end
+        function _.setDisabled(disabled) _.data.disabled = disabled return _ end
+        function _.setMaxValues(maxValues) _.data.maxValues = maxValues return _ end
+        function _.setMinValues(minValues) _.data.minValues = minValues return _ end
+        function _.setOptions(options) _.data.options = options return _ end
+        function _.setPlaceholder(placeholder) _.data.placeholder = placeholder return _ end
+        
+        function _.build() return _.data end
+
+        return _
+    end,
+    ButtonBuilder = function(data)
+        local _ = {
+            data = data or {}
+        }
+        _.data.type = 2
+
+        function _.setCustomId(customId) _.data.customId = customId return _ end
+        function _.setDisabled(disabled) _.data.disabled = disabled return _ end
+        function _.setEmoji(emoji) _.data.emoji = emoji return _ end
+        function _.setLabel(label) _.data.label = label return _ end
+        function _.setStyle(style)
+            assert(buttonStyles[style:upper()], "button style not found, valid options: PRIMARY, SECONDARY, SUCCESS, DANGER, LINK")
+            _.data.style = buttonStyles[style:upper()]
+            return _
+        end
+        function _.setURL(url) _.data.url = url return _ end
+        
+        function _.build() return _.data end
+
+        return _
+    end
+}
 
 local charset = {}  do -- [0-9a-zA-Z]
     for c = 48, 57  do table.insert(charset, string.char(c)) end
@@ -47,6 +151,8 @@ local function connect(_, retryCount)
     end
 end
 
+-- This listens to general messages, relaying it as its own event or as a reply event if it contains a nonce value
+-- This also will attempt to reconnect when the WS is closed.
 local function listen(_)
     while true do
         local e, url, msg = os.pullEvent()
@@ -58,7 +164,11 @@ local function listen(_)
                     if json.nonce then
                         os.queueEvent("websocket_reply", json.nonce, json)
                     elseif json.type then
-                        os.queueEvent("ccd_" .. json.type, json)
+                        if json.type == "button" or json.type == "selectMenu" then
+                            os.queueEvent("ccd_" .. json.type, json.customId, json)
+                        else
+                            os.queueEvent("ccd_" .. json.type, json)
+                        end
                     end
                 end
             elseif e == "websocket_closed" then
@@ -70,11 +180,41 @@ local function listen(_)
     end
 end
 
+-- This handles events registered by #.on()
 local function relay(_)
     while true do
-        local e, url, arg1, arg2, arg3, arg4, arg5, arg6, arg7 = os.pullEvent()
+        local e, arg1, arg2, arg3, arg4, arg5, arg6, arg7 = os.pullEvent()
 
         e = e:gsub("ccd_", "")
+
+        if e == "button" or e == "selectMenu" then
+            local replyId = arg2.replyId
+            arg2 = arg2.interaction
+            arg2.reply = function(message)
+                return _.send("interactionReply", {id = replyId, message = message})
+            end
+
+            arg2.replyEmbed = function(title, message, color)
+                return _.send("interactionReply", {id = replyId, message = {embeds = {CCD.EmbedBuilder().setTitle(title).setDescription(message).setColor(color).build()}}})
+            end
+
+            arg2.success = function(message)
+                if type(message) ~= "string" then
+                    return arg2.reply(message)
+                end
+
+                return arg2.replyEmbed("Success!", message, 0x31ad67)
+            end
+
+            arg2.error = function(message)
+                if type(message) ~= "string" then
+                    return arg2.reply(message)
+                end
+
+                return arg2.replyEmbed("Error!", message, 0xe83b3b)
+            end
+        end
+
         if _.listeners[e] then
             for i,v in pairs(_.listeners[e]) do
                 local status, err = pcall(function() v(arg1, arg2, arg3, arg4, arg5, arg6, arg7) end)
@@ -84,10 +224,20 @@ local function relay(_)
     end
 end
 
-function CCD.create(private, url, retryInterval, maxRetries, nonceLength)
+local function keepAlive(_)
+    while true do
+        if _.ws then
+            _.ws.send("{\"type\":\"keepAlive\"}")
+        end
+        sleep(10)
+    end
+end
+
+function CCD.new(private, url, retryInterval, maxRetries, nonceLength)
     assert(type(private) == "string" and #private == 64, "invalid private key. must be a 64 character string")
 
     local _ = {
+        builders = CCD.builders,
         private = private,
         url = url or WS_URL,
         retryInterval = retryInterval or RETRY_INTERVAL,
@@ -104,7 +254,7 @@ function CCD.create(private, url, retryInterval, maxRetries, nonceLength)
     end
 
     function _.listen()
-        parallel.waitForAny(function() listen(_)end, function() relay(_)end)
+        parallel.waitForAny(function() listen(_)end, function() relay(_)end, function() keepAlive(_)end)
     end
 
     function _.send(msgType, msg)
@@ -130,14 +280,19 @@ function CCD.create(private, url, retryInterval, maxRetries, nonceLength)
     _.users = {
         get = function(userId)
             local msg = _.send("getUser", {id = userId})
-            print(msg.user.tag)
             return msg.success, msg
         end,
         send = function(userId, message)
             local msg = _.send("send", {recipient = userId, message = message})
-            print(textutils.serialize(msg))
             return msg.success, msg
-        end
+        end,
+    }
+
+    _.channels = {
+        send = function(guildId, channelId, message)
+            local msg = _.send("send", {guild = guildId, channel = channelId, message = message})
+            return msg.success, msg
+        end,
     }
 
     function _.on(event, func)
